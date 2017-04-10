31c100739d23faad19b17bde28645dfe347eac6b
X Coan <xjcoan@gmail.com>
2017-04-10 17:59:10 -0400

    Convert to API with routes.

--- readme.md
+++ readme.md
@@ -1,3 +1,25 @@
 # Learning Mongo and Node
+### A REST API built in Node with Mongo.
 
-This was built following [Zellwk's Tutorial](https://zellwk.com/blog/crud-express-mongodb/).
+
+This is based on [Zellwk's Tutorial](https://zellwk.com/blog/crud-express-mongodb/).
+
+
+## API Features
+* Create
+* Read
+* Delete
+
+GET `/quotes/json` returns json for quotes
+GET `/quotes/:id/json` returns json of specific quote
+GET `/quotes/:id/name` returns name of specified quote
+GET `/quotes/:id/quote` returns the quote itself
+
+POST `/quotes/:name/:quote` Posts a new quote to the DB
+
+DELETE `/quotes/:id` removes the quote from the DB
+
+## TODO
+* Add Update functionality
+* Make POST route less janky
+* Add auth?
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+var ObjectId = require('mongodb').ObjectId
 
 const app = express()
 
@@ -11,9 +12,11 @@ app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.use(express.static('public'))
+
 var db
 
-MongoClient.connect(`mongodb://${mongoUser}:${mongoPass}@ds131320.mlab.com:31320/learn-mongo-db`, (err, database) => {
+MongoClient.connect(`mongodb://dbuser:dbpass@ds131320.mlab.com:31320/learn-mongo-db`, (err, database) => {
   if (err) return console.log(err)
   db = database
   app.listen(3000, () => {
@@ -21,18 +24,81 @@ MongoClient.connect(`mongodb://${mongoUser}:${mongoPass}@ds131320.mlab.com:31320
   })
 })
 
-app.get('/', (req, res) => {
+app.get('/', (req,res) => {
+  res.send('you in the wrong neighborhood, muthafucka!')
+})
+
+app.get('/quotes', (req, res) => {
   db.collection('quotes').find().toArray((err, result) => {
     if (err) return console.log(err)
-    res.render('index.ejs', {quotes: result})
+    res.render('quotes.ejs', {quotes: result})
+  })
+})
+
+app.get('/quotes/json', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.send(result)
+  })
+})
+
+app.get('/quotes/:q_id', (req, res) => {
+  db.collection('quotes').find({'_id': new ObjectId(req.params.q_id)}).toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('quote.ejs', {quote: result})
+    console.log(result[0])
+  })
+})
+
+app.get('/quotes/:q_id/name', (req, res) => {
+  db.collection('quotes').find({'_id': new ObjectId(req.params.q_id)}).toArray((err, result) => {
+    if (err) return console.log(err)
+    res.send(result[0].name)
+    console.log(result[0].name)
+  })
+})
+
+app.get('/quotes/:q_id/quote', (req, res) => {
+  db.collection('quotes').find({'_id': new ObjectId(req.params.q_id)}).toArray((err, result) => {
+    if (err) return console.log(err)
+    res.send(result[0].quote)
+    console.log(result[0].quote)
+  })
+})
+
+app.get('/quotes/:q_id/json', (req, res) => {
+  db.collection('quotes').find({'_id': new ObjectId(req.params.q_id)}).toArray((err, result) => {
+    if (err) return console.log(err)
+    res.json(result)
+    console.log(result)
+  })
+})
+
+app.post('/quotes/:name/:quote', (req, res) => {
+  var new_name = req.params.name
+  var new_quote = req.params.quote
+  db.collection('quotes').save({ name: new_name, quote: new_quote}, (err, result) => {
+    if (err) return console.log(err)
+
+    res.send(result)
+    console.log(result)
   })
 })
 
 app.post('/quotes', (req, res) => {
+  console.log(req)
     db.collection('quotes').save(req.body, (err, result) => {
     if (err) return console.log(err)
 
-    console.log('saved to database')
-    res.redirect('/')
+    console.log(req.body)
+    res.send(result)
+  })
+})
+
+app.delete('/quotes/:q_id', (req, res) => {
+  db.collection('quotes').remove({'_id': ObjectId(req.params.q_id)}, (err, result) => {
+    if (err) return console.log(err)
+    res.json(result)
+    console.log(result)
   })
 })
